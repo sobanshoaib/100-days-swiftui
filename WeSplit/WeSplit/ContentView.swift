08077d2d69cd2c4eba48cbc1b7343b41f1942ea2
Soban Shoaib <sobanshoaib7500@gmail.com>
2026-03-08 18:40:53 -0600

    first-project

---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,89 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Soban Shoaib on 2026-03-05.
+//
+
+import SwiftUI
+
+struct ContentView: View { 
+    @State var costCheque = 0.0
+    @State var numPeople = 2
+    @State var tipAmount = 20
+    @FocusState private var amountFocused: Bool
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        
+        let peopleCount = Double(numPeople)
+        let tipSelection = Double(tipAmount)
+        
+        
+        let tipValue = costCheque / 100 * tipSelection
+        let grandTotal = costCheque + tipValue
+        let amountPerson = grandTotal / peopleCount
+        
+        return amountPerson
+    }
+    
+    var totalAfterTip: Double {
+        
+        let tipSelection = Double(tipAmount)
+        let tipValue = costCheque / 100 * tipSelection
+        let grandTotal = costCheque + tipValue
+        
+        return grandTotal
+    }
+    
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Amount", value: $costCheque, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                        .focused($amountFocused)
+                    
+                    Picker("Number of people", selection: $numPeople) {
+                        ForEach(0..<100) { num in
+                            Text("\(num) people")
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                }
+                
+                Section ("How much tip do you want to leave") {
+                    Picker("Tip percentage", selection: $tipAmount) {
+                        ForEach(tipPercentages, id: \.self) { tip in
+                            Text(tip, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                
+                
+                
+                Section ("Amount per person") {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+                
+                Section("Total amount before dividing") {
+                    Text(totalAfterTip, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+            }
+            .navigationTitle("WeSplit")
+            .toolbar {
+                if amountFocused {
+                    Button("Done") {
+                        amountFocused = false
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Soban Shoaib on 2026-03-05.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
